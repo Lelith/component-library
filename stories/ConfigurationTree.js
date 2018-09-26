2d9f8e1b81386bad6d50904ec4bc691619336358
Aleksander Barszczewski <alek.barszczewski@gmail.com>
2018-09-26 17:04:53 +0200

    config tree WIP

---
+++ stories/ConfigurationTree.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+import { ThemeProvider, ConfigurationTree } from '../lib';
+
+const wrapperStyle = {
+  padding: '15px',
+};
+
+const containerStyle = {
+  width: '50%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+};
+
+const treeProps = {
+  nodes: [
+    {},
+    {},
+    {},
+    {},
+    {},
+    // {},
+    // {},
+    // {},
+    // {},
+    // {},
+    // {},
+    // {},
+  ],
+};
+
+storiesOf('D3A/Organisms/ConfigurationTree', module)
+  .add(
+    'Default',
+    withInfo(`
+      ConfigurationTree
+
+      ~~~js
+      <ConfigurationTree />
+      ~~~
+
+    `)(() => (
+      <ThemeProvider theme="d3a">
+        <div className="base" style={wrapperStyle}>
+          <div style={containerStyle}>
+            <ConfigurationTree {...treeProps} />
+          </div>
+        </div>
+      </ThemeProvider>
+    )),
+  );
