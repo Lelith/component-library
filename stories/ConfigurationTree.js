85bbc03c4fc4f25f2f3098199e3fc9a66b1e05ef
Aleksander Barszczewski <alek.barszczewski@gmail.com>
2018-10-23 16:58:42 +0200

    depth control

--- stories/ConfigurationTree.js
+++ stories/ConfigurationTree.js
@@ -16,52 +16,84 @@ const containerStyle = {
 
 const treeProps = {
   nodes: [
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
-    { type: 'PV', name: 'PV' },
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
     {
-      type: 'InfiniteDieselGenerator',
-      name: 'Diesel',
-    },
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
-    {
-      type: 'Load',
-      name: 'Load',
+      type: 'Area',
+      name: 'Area1',
       active: true,
       nodes: [
-        // { type: 'Area', name: 'Area' },
-        // { type: 'Storage', name: 'Storage' },
-        // { type: 'PV', name: 'PV' },
-        // { type: 'Storage', name: 'Storage' },
-        // { type: 'PV', name: 'PV' },
-        // { type: 'Storage', name: 'Storage' },
-        // {
-        //   type: 'Area',
-        //   name: 'Area',
-        //   active: false,
-        //   nodes: [{ type: 'Add', active: false }], /* { type: 'Area', name: 'Area', test: true }, { type: 'Area', name: 'Area', active: false }, */
-        // },
-        // { type: 'PV', name: 'PV' },
-        // { type: 'PV', name: 'PV' },
-        { type: 'Add', active: false },
+        {
+          type: 'Area',
+          name: 'Area2',
+          active: true,
+          nodes: [
+            {
+              type: 'Area',
+              name: 'Area3',
+              active: true,
+              nodes: [
+                {
+                  type: 'Area',
+                  name: 'Area2',
+                  active: true,
+                  nodes: [
+                    {
+                      type: 'Area',
+                      name: 'Area3',
+                      active: true,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
       ],
     },
-    { type: 'PV', name: 'PV' },
-    { type: 'Storage', name: 'Storage' },
-    {
-      type: 'Add',
-    },
+    // { type: 'Storage', name: 'Storage' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // {
+    //   type: 'InfiniteDieselGenerator',
+    //   name: 'Diesel',
+    // },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // {
+    //   type: 'Load',
+    //   name: 'Load',
+    //   active: true,
+    //   nodes: [
+    // { type: 'Area', name: 'Area' },
+    // { type: 'Storage', name: 'Storage' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // {
+    //   type: 'Area',
+    //   name: 'Area',
+    //   active: false,
+    //   nodes: [{ type: 'Add', active: false }], /* { type: 'Area', name: 'Area', test: true }, { type: 'Area', name: 'Area', active: false }, */
+    // },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'PV', name: 'PV' },
+    //     { type: 'Add', active: false },
+    //   ],
+    // },
+    // { type: 'PV', name: 'PV' },
+    // { type: 'Storage', name: 'Storage' },
+    // {
+    //   type: 'Add',
+    // },
   ],
 };
 
